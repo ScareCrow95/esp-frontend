8dd3e632f89a4ea1a7106a1adfdffe246571028f
scarecrow95 <mridul@raincrowgames.com>
2021-09-26 17:18:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Box, Text, Center, Flex, Image, Circle, Icon } from '@chakra-ui/react'
+import * as mqtt from 'react-paho-mqtt'
+import { Line } from 'react-chartjs-2'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data, setData] = useState({
+        labels: [],
+        datasets: [
+            {
+                label: 'Temperature',
+                backgroundColor: 'rgba(68, 102, 242, 0.4)',
+                borderColor: '#4466f2',
+                pointBackgroundColor: 'rgba(68, 102, 242, 0.4)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(68, 102, 242, 0.4)',
+                data: [],
+            },
+        ],
+    })
+
+    const lineChartOptions = {
+        maintainAspectRatio: true,
+        legend: {
+            display: false,
+        },
+        scales: {
+            xAxes: [
+                {
+                    display: false,
+                },
+            ],
+            yAxes: [
+                {
+                    suggestedMin: 60,
+                },
+            ],
+        },
+    }
+
+    useEffect(() => {
+        const client = mqtt.connect(
+            'broker.hivemq.com',
+            Number(8000),
+            'mqtt',
+            () => {},
+            ({ payloadString }) => {
+                const temp = { ...data }
+
+                temp.labels.push(new Date().toLocaleTimeString('en-US'))
+                temp.datasets[0].data.push(parseFloat(payloadString))
+                setData(temp)
+            },
+        )
+        client.connect({
+            onSuccess: () => {
+                client.subscribe('pareek/temp')
+            },
+        })
+    })
+    return (
+        <Flex direction="column" w="100vw" h="100vh" bg="gray.900" justify="center" align="center">
+            <Text fontSize="3xl">ESP32 Temperature Realtime Data</Text>
+            <Text fontSize="xl" mb={8} color="blue.400">
+                1000ms
+            </Text>
+            <Box w="70%" h="20%">
+                <Line data={data} options={lineChartOptions} width={300} height={60} />
+            </Box>
+            <Flex position="absolute" bottom="0px" right="0px" m={8} color="gray.300">
+                <Text mr={1}>Mridul Pareek</Text>
+                <Text color="blue.400">• Physics 387 </Text>
+            </Flex>
+        </Flex>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export const darkTheme = ReactDOM.render(
+    <React.StrictMode>
+        <ChakraProvider
+            theme={extendTheme({
+                components: { Button: { baseStyle: { _focus: { boxShadow: 'none' } } } },
+                shadows: { outline: '0 !important' },
+                config: {
+                    useSystemColorMode: false,
+                    initialColorMode: 'dark',
+                },
+            })}
+        >
+            {/* <Connector
+                mqttProps={{
+                    url: 'ws://test.mosquitto.org:8080',
+                    options: { protocol: 'ws' }, // see MQTTjs options
+                }}
+            > */}
+            <App />
+            {/* </Connector> */}
+        </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById('root'),
+)
